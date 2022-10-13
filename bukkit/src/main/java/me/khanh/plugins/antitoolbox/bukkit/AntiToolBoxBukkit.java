785c2eaa045a54812fb8e62b1def034e3e41f319
Huỳnh Khánh <khanh@duck.com>
2022-10-13 22:06:10 +0700

    Initial commit

---
+++ bukkit/src/main/java/me/khanh/plugins/antitoolbox/bukkit/AntiToolBoxBukkit.java
@@ -0,0 +1,17 @@
+package me.khanh.plugins.antitoolbox.bukkit;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class AntiToolBoxBukkit extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
---
+++ bungee/src/main/java/me/khanh/plugins/antitoolbox/bungee/AntiToolBoxBungee.java
@@ -0,0 +1,16 @@
+package me.khanh.plugins.antitoolbox.bungee;
+
+import net.md_5.bungee.api.plugin.Plugin;
+
+public final class AntiToolBoxBungee extends Plugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
